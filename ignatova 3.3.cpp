02e8b85715c0278fa126ba0679665f11eea11e35
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-21 14:08:32 +0300

    практика 3

---
+++ ignatova 3.3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "Введите размер массива: ";
+    cin >> n;
+
+    int arr[3];
+    cout << "Введите элементы массива:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Содержимое массива:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    int sum = 0;
+    int count = 0;
+    int k = 3;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % k == 0) {
+            sum += arr[i];
+            count++;
+        }
+    }
+    cout << "Сумма элементов массива кратных " << k << " равна: " << sum << endl;
+    cout << "Количество элементов массива кратных " << k << " равно: " << count << endl;
+
+    return 0;
+}
