267f926c42482f0fcc4335c3db25b37801e8579d
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-21 13:26:08 +0300

    практика 5

---
+++ ignatova 3.5.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int height = 10;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < height - i; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < i * 2 + 1; j++) {
+			cout << "^";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < height; i++) {
+		cout << " =";
+	}
+	cout << endl;
+	for (int i = 0; i < height - 2; i++) {
+		cout << " | ";
+		for (int j = 0; j < height - 2; j++) {
+			cout << " ";
+		}
+		cout << "|" << endl;
+	}
+	for (int i = 0; i < height; i++) {
+		cout << " =";
+	}
+	cout << endl;
+	return 0;
+}
+
