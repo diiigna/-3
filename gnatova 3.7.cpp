f991cd8f39a6b2349708ec7235ea15cf545fa74d
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-21 09:51:54 +0300

    практика 7

---
+++ gnatova 3.7.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long zadacha = 1;
+	const int primer = 10;
+	for (int i = 1; 1 <= primer; i++)
+		zadacha *= (i * (i + 1)) / 2;
+
+	cout << zadacha << endl;
+
+	return 0;
+}
+
