febf294b826346d7b951083eb91e22ccb25d5855
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-21 13:01:41 +0300

    практика 9

---
+++ ignatova 3.9.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    cout << "Выберите фигуру: " << endl;
+    cout << "[1] Треугольник" << endl;
+    cout << "[2] Трапеция" << endl;
+    int figure; cin >> figure;
+
+    cout << "Введите высоту: " << endl;
+    int height; cin >> height;
+    if (figure == 1) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height - i - 1; j++) {
+                cout << " ";
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+    else if (figure == 2) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height - i - 1; j++) {
+                cout << " ";
+            }
+            for (int k = 0; k < 2 * (i + 1) + 1; k++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+}
